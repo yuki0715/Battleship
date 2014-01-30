577b36d4244dd91f55c0c69b01dce784abd22c87
metalconker <metal.conker@gmail.com>
2014-01-30 02:45:45 -0500

    Empty Battleship SpriteKit project committed

---
+++ Battleship/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Battleship
+//
+//  Created by Rayyan Khoury on 1/30/2014.
+//  Copyright (c) 2014 Rayyan Khoury. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ BattleshipTests/BattleshipTests.m
@@ -0,0 +1,34 @@
+//
+//  BattleshipTests.m
+//  BattleshipTests
+//
+//  Created by Rayyan Khoury on 1/30/2014.
+//  Copyright (c) 2014 Rayyan Khoury. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface BattleshipTests : XCTestCase
+
+@end
+
+@implementation BattleshipTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
